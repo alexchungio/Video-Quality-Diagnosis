e68d3ea2b9ccd88d12582ecfdabaa810f116451d
alexchung <yonganzhong@outlook.com>
2020-11-26 14:31:36 +0800

    add opencv dft operation

--- dft.py
+++ dft.py
@@ -15,7 +15,7 @@ import numpy as np
 from scipy import ndimage
 import cv2 as cv
 
-from tools import visual_fft_spectrum, visual_fft_phase
+from tools import visual_fft_magnitude, visual_fft_phase, visual_magnitude_with_opencv
 
 
 barbara_path = './images/DIP3E_CH4/Fig0417(a)(barbara).tif'
@@ -49,28 +49,28 @@ def visual_fft():
     # #----------------------------------- change spectrum amplitude--------------------------
     # scale_f = np.log(1 + np.abs(central_f))
     # visual_fft_spectrum_demo(scale_f, 'scale fft spectrum')
-    visual_fft_spectrum(f, 'origin fft spectrum')
+    visual_fft_magnitude(f, 'origin fft spectrum')
     visual_fft_phase(f, 'origin fft phase')
 
     # ----------------------------------- translate-----------------------------------------
     translated_img = plt.imread(translated_rectangle_path)
     plt.imshow(translated_img, cmap='gray')
     translated_f = np.fft.fft2(translated_img)
-    visual_fft_spectrum(translated_f, 'translated fft spectrum')
+    visual_fft_magnitude(translated_f, 'translated fft spectrum')
     visual_fft_phase(translated_f, 'translated fft phase')
     #----------------------------------- image rotation--------------------------------------
     rotation_img = ndimage.rotate(gray_img, angle=45)
     plt.imshow(rotation_img, cmap='gray')
     rotation_f = np.fft.fft2(rotation_img)
-    visual_fft_spectrum(rotation_f, 'rotation fft spectrum')
+    visual_fft_magnitude(rotation_f, 'rotation fft spectrum')
     visual_fft_phase(rotation_f, 'translated fft phase')
     plt.show()
     print('Done')
 
 
-def blur_with_low_pass(image, size=30):
+def blur_with_numpy(image, size=30):
     """
-
+    low_pass
     :param image:
     :param size:
     :return:
@@ -139,12 +139,72 @@ def sharp_with_high_pass(image, size=30, alpha=0.85):
     return sharp_img, mask
 
 
-def main():
-    gray_imag = cv.imread(barbara_path, cv.IMREAD_GRAYSCALE)
-    sharp_img, high_mask = sharp_with_high_pass(gray_imag)
-    plt.imshow(sharp_img, cmap='gray')
-    plt.show()
+def blur_with_opencv(image, size=30):
 
+    assert len(image.shape) == 2
+
+    # -----------------Expand the image to an optimal size with padding-------------------
+    rows, cols = image.shape
+    m = cv.getOptimalDFTSize(rows)
+    n = cv.getOptimalDFTSize(cols)
+    padded = cv.copyMakeBorder(image, 0, m - rows, 0, n - cols, cv.BORDER_CONSTANT, value=[0, 0, 0])
+
+    # -------------------------Make place for both the complex and the real values------------------
+    planes = [np.float32(padded), np.zeros(padded.shape, np.float32)]
+    complex_img = cv.merge(planes)  # Add to the expanded another plane with zeros
+
+    # ---------------------construct mask----------------------------
+    center_y, center_x = int(complex_img.shape[0] / 2), int(complex_img.shape[1] / 2)
+    mask = np.zeros_like(complex_img, dtype=np.uint8)
+    mask[center_y - size:center_y + size, center_x - size:center_x + size, :] = 1
+
+    # -------------------------Make the Discrete Fourier Transform-----------------------------------
+    dft_img = cv.dft(complex_img)  # this way the result may fit in the source matrix
+
+    # -------------------------ransform the real and complex values to magnitude--------------------
+    # compute the magnitude and switch to logarithmic scale
+    # = > M = sqrt(Re(DFT(I)) ^ 2 + Im(DFT(I)) ^ 2)
+    # cv.split(complexI, planes)  # planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+    # cv.magnitude(planes[0], planes[1], planes[0])  # planes[0] = magnitude
+    # image_ = planes[0]
+
+    shift_img = np.fft.fftshift(dft_img)
+    shift_img *= mask
+
+    # invert centralize
+    ishift_img = np.fft.ifftshift(shift_img)
+    ifft_img = cv.idft(ishift_img)
+
+    blur_img, img_1 = cv.split(ifft_img)
+
+    # convert scale to (0, 1)
+    cv.normalize(blur_img, blur_img, 0, 1, cv.NORM_MINMAX)
+
+    # recover origin shape
+    blur_img = blur_img[0:rows, 0:cols]
+
+    return blur_img
+
+# def main():
+#     gray_imag = cv.imread(barbara_path, cv.IMREAD_GRAYSCALE)
+#     sharp_img, high_mask = sharp_with_high_pass(gray_imag)
+#     plt.imshow(sharp_img, cmap='gray')
+#     plt.show()
+
+def main():
+
+    image = cv.imread(barbara_path, cv.IMREAD_GRAYSCALE)
+    if image is None:
+        print('Error opening image')
+        return -1
+
+    # mag_img = visual_magnitude_with_opencv(image)
+    # [normalize]
+    blur_img = blur_with_opencv(image)
+    cv.imshow("Input Image"       , image)    # Show the result
+    cv.imshow("spectrum magnitude", blur_img)
+    cv.waitKey()
 
 if __name__ == "__main__":
     main()
+    # print(100 & -2)
--- tools.py
+++ tools.py
@@ -50,7 +50,7 @@ def show_histogram(image, title='hist'):
         plt.show()
 
 
-def visual_fft_spectrum(fft, title='fft_spectrum'):
+def visual_fft_magnitude(fft, title='fft_magnitude'):
     """
 
     :param spectrum:
@@ -94,3 +94,60 @@ def visual_fft_phase(fft, title='fft_phase'):
     plt.title(title)
     plt.imshow(scale_f, cmap='gray')
     plt.show()
+
+
+def visual_magnitude_with_opencv(image):
+    """"""
+    assert len(image.shape) == 2
+    # -----------------Expand the image to an optimal size with padding-------------------
+    rows, cols = image.shape
+    m = cv.getOptimalDFTSize(rows)
+    n = cv.getOptimalDFTSize(cols)
+    padded = cv.copyMakeBorder(image, 0, m - rows, 0, n - cols, cv.BORDER_CONSTANT, value=[0, 0, 0])
+
+    # -------------------------Make place for both the complex and the real values------------------
+    planes = [np.float32(padded), np.zeros(padded.shape, np.float32)]
+    complex_img = cv.merge(planes)  # Add to the expanded another plane with zeros
+
+    # -------------------------Make the Discrete Fourier Transform-----------------------------------
+    dft_img = cv.dft(complex_img)  # this way the result may fit in the source matrix
+
+    # ---------------------compute the magnitude and switch to logarithmic scale-----------------------
+    # = > log(1 + sqrt(Re(DFT(I)) ^ 2 + Im(DFT(I)) ^ 2))
+    ## [magnitude]
+    cv.split(dft_img, planes)  # planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+    mag_img = cv.magnitude(planes[0], planes[1])  # planes[0] = magnitude
+
+    # ------------------------Switch to a logarithmic scale-----------------------------------------
+    # = > log(1 + M)
+    mat_ones = np.ones(mag_img.shape, dtype=mag_img.dtype)
+    cv.add(mat_ones, mag_img, mag_img)  # switch to logarithmic scale
+    cv.log(mag_img, mag_img)  # log
+
+    # ------------------------Centralize----------------------------------------------------
+
+    # magI_rows, magI_cols = magI.shape
+    # # crop the spectrum, if it has an odd number of rows or columns
+    # magI = magI[0:(magI_rows & -2), 0:(magI_cols & -2)]  # convert to even
+    # cx = int(magI_rows/2)
+    # cy = int(magI_cols/2)
+    #
+    # q0 = magI[0:cx, 0:cy]         # Top-Left - Create a ROI per quadrant
+    # q1 = magI[cx:cx+cx, 0:cy]     # Top-Right
+    # q2 = magI[0:cx, cy:cy+cy]     # Bottom-Left
+    # q3 = magI[cx:cx+cx, cy:cy+cy] # Bottom-Right
+    #
+    # tmp = np.copy(q0)               # swap quadrants (Top-Left with Bottom-Right)
+    # magI[0:cx, 0:cy] = q3
+    # magI[cx:cx + cx, cy:cy + cy] = tmp
+    #
+    # tmp = np.copy(q1)               # swap quadrant (Top-Right with Bottom-Left)
+    # magI[cx:cx + cx, 0:cy] = q2
+    # magI[0:cx, cy:cy + cy] = tmp
+    mag_img = np.fft.fftshift(mag_img)
+
+    # --------------------------------Normalize--------------------------------------------
+    # Transform the matrix with float values into a viewable image form(float between values 0 and 1).
+    cv.normalize(mag_img, mag_img, 0, 1, cv.NORM_MINMAX)
+
+    return mag_img
