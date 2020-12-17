f18b6191962f954230730bdb54a5d95ae0752bb0
alexchung <yonganzhong@outlook.com>
2020-12-17 09:19:03 +0800

    add video_stability module

---
+++ video_stability.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : video_stability.py
+# @ Description:  https://www.learnopencv.com/video-stabilization-using-point-feature-matching-in-opencv/
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2020/12/17 上午9:18
+# @ Software   : PyCharm
+#-------------------------------------------------------
